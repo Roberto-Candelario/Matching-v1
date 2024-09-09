136db4c5616c1d268ab62719cfc33db211d5c82b
Roberto Candelario <r-jc@live.com>
2024-09-09 16:54:02 -0400

    Finished Assignment 1

---
+++ Matching v1/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Matching v1
+//
+//  Created by Roberto Candelario on 9/9/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
