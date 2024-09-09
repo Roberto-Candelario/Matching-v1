5b567591a7318da21334dfbb7c3894f66baa2552
Roberto Candelario <r-jc@live.com>
2024-09-09 14:41:18 -0400

    Initial Commit

---
+++ Matching v1/Matching_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Matching_v1App.swift
+//  Matching v1
+//
+//  Created by Roberto Candelario on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Matching_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
